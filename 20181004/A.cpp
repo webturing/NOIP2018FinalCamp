7d2efe02551d1c41a879841c0cea90bf09d736cd
jal <517486222@qq.com>
2018-10-04 15:43:24 +0800

    NOIP第四天 A题

--- 20181004/A.cpp
+++ 20181004/A.cpp
@@ -5,12 +5,17 @@
 #include <bits/stdc++.h>
 using namespace std;
 int main(){
+    ofstream cout("A.out");
     int n;
     while(cin>>n){
+        int sum[n+1] = {0};
+        for(int i = 1; i <= n; i++){
+            sum[i] = sum[i-1]+i;
+        }
         for(int i = 1; i < n; i++){
-            int a = i;
-            if((2*a+1)*(2*a+1) - 4 *(2*a-2*n) == 0){
-
+            int x = sum[i-1]+n;
+            if(binary_search(sum+i, sum+n+1, x)){
+                cout << i << " " << lower_bound(sum+i, sum+n+1, x) - sum<<endl;
             }
         }
     }
